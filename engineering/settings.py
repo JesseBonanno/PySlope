d36f97117c2827d8e81892e7a499c3cb9f3c35b5
JesseBonanno <53243847+JesseBonanno@users.noreply.github.com>
2022-05-02 18:29:04 +1000

    Update settings.py

--- engineering/settings.py
+++ engineering/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'engineering.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
