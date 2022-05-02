541324d14eea2a1033c90e61b15d70b6b4d1e78c
JesseBonanno <53243847+JesseBonanno@users.noreply.github.com>
2022-05-02 20:15:28 +1000

    Revert "Update settings.py"
    This reverts commit 32a7eea86fc251eeb446463d623276306463cd68.

--- engineering/settings.py
+++ engineering/settings.py
@@ -27,10 +27,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 if DEBUG:
-    ALLOWED_HOSTS = ['https://pyslope.herokuapp.com/','.pyslope.com','pyslope.herokuapp.com']
+    ALLOWED_HOSTS = []
 else:
     ALLOWED_HOSTS = ['https://pyslope.herokuapp.com/','.pyslope.com','pyslope.herokuapp.com']
 
