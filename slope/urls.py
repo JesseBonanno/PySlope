aa3b5138f54818ea45a80054e811712e1a2b4e3c
JesseBonanno <53243847+JesseBonanno@users.noreply.github.com>
2022-05-26 19:22:12 +1000

    pdf processing
    changed the ways pdf's are returned.

--- slope/urls.py
+++ slope/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name = 'index'),
-    path('reset', views.reset, name = 'reset'),
+    path("", views.index, name="index"),
+    path("reset", views.reset, name="reset"),
+    path("pdf/<str:max_fos>", views.pdf, name="pdf"),
 ]
